56c9bdb40a580c6479d131449b252ba07dc00b2f
David Kuruvilla <dapawn@gmail.com>
2017-11-02 13:41:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   def initialize(side1,side2,side3)
-    if side1 <= 0 || side2 <=0 || side3 <= 0
+    if (side1 <= 0 || side2 <=0 || side3 <= 0)
       begin
         raise TriangleError
       rescue TriangleError => error
