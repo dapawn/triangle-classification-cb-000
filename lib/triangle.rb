56ed16d1dd7befb56ad4aff17c979e58066a8f48
David Kuruvilla <dapawn@gmail.com>
2017-11-02 13:39:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,9 +14,9 @@ class Triangle
   end
 
   def kind
-    if (side1 == side2 && side2 == side3)
+    if (@side1 == @side2 && @side2 == @side3)
       :equilateral
-    elsif (side1 == side2 || side2 == side3 || side1 == side3)
+    elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
       :isosceles
     else
       :scalene
