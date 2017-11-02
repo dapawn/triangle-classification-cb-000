67712e1488d5950f34c0aaf88e41ce85462e5091
David Kuruvilla <dapawn@gmail.com>
2017-11-02 14:00:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,22 +1,24 @@
+require "pry"
 class Triangle
   def initialize(side1,side2,side3)
-    if (side1 <= 0 || side2 <=0 || side3 <= 0)
+    @sides[0] = side1
+    @sides[1] = side2
+    @sides[2] = side3
+    @sides.sort!
+    #binding.pry
+    if (@sides[0] <= 0 || (@sides[0] + @sides[1] <= @sides[2]))
       begin
         raise TriangleError
       rescue TriangleError => error
         puts error.message
       end
-    else
-      @side1 = side1
-      @side2 = side2
-      @side3 = side3
     end
   end
 
   def kind
-    if (@side1 == @side2 && @side2 == @side3)
+    if (@sides[0] == @sides[1] && @sides[1] == @sides[2])
       :equilateral
-    elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
+    elsif (@sides[1] == @sides[2])
       :isosceles
     else
       :scalene
